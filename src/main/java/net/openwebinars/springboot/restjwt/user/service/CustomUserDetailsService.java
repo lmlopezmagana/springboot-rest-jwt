470ffe844f76bb7b0293c35ca264bee338ed808f
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-11-09 13:21:47 +0100

    v0.3-persistencia

---
+++ src/main/java/net/openwebinars/springboot/restjwt/user/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package net.openwebinars.springboot.restjwt.user.service;
+
+
+import lombok.RequiredArgsConstructor;
+import net.openwebinars.springboot.restjwt.user.repo.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with username: " +  username));
+    }
+}
