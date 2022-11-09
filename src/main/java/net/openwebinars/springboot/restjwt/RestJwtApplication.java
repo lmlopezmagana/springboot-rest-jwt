9a8d8c1eb593d25c2ecc4341a2d010e634629410
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-11-09 11:06:59 +0100

    Proyecto

---
+++ src/main/java/net/openwebinars/springboot/restjwt/RestJwtApplication.java
@@ -0,0 +1,13 @@
+package net.openwebinars.springboot.restjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/openwebinars/springboot/restjwt/RestJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package net.openwebinars.springboot.restjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
