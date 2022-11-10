0f7e4518b49e8bc63db2fee9172b1bb9c42944c9
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-11-10 12:57:39 +0100

    Minor bugs fixed

--- src/main/java/net/openwebinars/springboot/restjwt/user/model/User.java
+++ src/main/java/net/openwebinars/springboot/restjwt/user/model/User.java
@@ -66,7 +66,7 @@ public class User implements UserDetails {
     @Builder.Default
     private boolean enabled = true;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     private Set<UserRole> roles;
 
     @CreatedDate
