9e14e58a3073e7d78b391d61b50e5c6b2ec09b5e
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-11-20 11:38:34 +0100

    v0.16-error-handling-update minor bugs fixed

--- src/main/java/net/openwebinars/springboot/restjwt/security/jwt/access/JwtAuthenticationFilter.java
+++ src/main/java/net/openwebinars/springboot/restjwt/security/jwt/access/JwtAuthenticationFilter.java
@@ -5,12 +5,15 @@ import lombok.extern.java.Log;
 import net.openwebinars.springboot.restjwt.security.errorhandling.JwtTokenException;
 import net.openwebinars.springboot.restjwt.user.model.User;
 import net.openwebinars.springboot.restjwt.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -27,6 +30,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final UserService userService;
     private final JwtProvider jwtProvider;
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
 
 
     @Override
@@ -61,7 +69,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         } catch (JwtTokenException ex) {
             log.info("Authentication error using token JWT: " + ex.getMessage());
-            throw ex;
+            resolver.resolveException(request, response, null, ex);
         }
 
 
