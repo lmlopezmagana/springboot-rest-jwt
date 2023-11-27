1fc31acf9e36d8f05cb9b90b241dae6777d73197
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-11-27 10:43:31 +0100

    Se ajusta la configuración de CORS para que aplique a peticions PUT y DELETE

--- src/main/java/net/openwebinars/springboot/restjwt/config/WebConfig.java
+++ src/main/java/net/openwebinars/springboot/restjwt/config/WebConfig.java
@@ -14,7 +14,9 @@ public class WebConfig
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
             }
         };
     }
